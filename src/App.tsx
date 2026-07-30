04dd56b296eb7871af9d891941e6a14033579f16
Jack4132 <jacksonthounaojam1@gmail.com>
2026-07-31 00:56:08 +1000

    bypass login remove

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Navigate, Route, Routes } from 'react-router-dom'
 import { AppShell } from './components/AppShell'
+import { ProtectedRoute } from './components/ProtectedRoute'
 import { DashboardPage } from './pages/DashboardPage'
 import { LoginPage } from './pages/LoginPage'
 import { PreviewPage } from './pages/PreviewPage'
@@ -20,7 +21,7 @@ function App() {
     <Routes>
       <Route path="/login" element={<LoginPage />} />
 
-      {/* <Route element={<ProtectedRoute />}> */}
+      <Route element={<ProtectedRoute />}>
         <Route element={<AppShell />}>
           <Route path="/dashboard" element={<DashboardPage />} />
           <Route path="/tracking" element={<DashboardPage />} />
@@ -30,7 +31,7 @@ function App() {
           <Route path="/tests/:testId/preview" element={<PreviewPage />} />
           <Route path="/" element={<Navigate to="/dashboard" replace />} />
         </Route>
-      {/* </Route> */}
+      </Route>
 
       <Route path="*" element={<NotFoundPage />} />
     </Routes>
